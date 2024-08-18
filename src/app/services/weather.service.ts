6ebe2756f9336113d3279519ee55f9602c19cb4c
nasriime <mnasr4040@gmail.com>
2024-08-18 15:15:05 +0200

    add multi days request + data types + change in environment variable

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { weatherData } from '../models/weather.model';
+import { multiDaysForcastData } from '../models/multiDaysForcast.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,10 +12,19 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string): Observable<weatherData> {
-    return this.http.get<weatherData>(`${environment.weatherApiBaseUrl}`,{
+  getCurrentWeather(cityName: string): Observable<weatherData> {
+    return this.http.get<weatherData>(`${environment.weatherApiBaseUrl}/weather`,{
       params: new HttpParams()
       .set('q', cityName)
+      .set('appid', environment.weatherApiKey)
+      .set('units', 'metric')
+    });
+  }
+  getMultiDaysForcast(lat: string, lon: string): Observable<multiDaysForcastData> {
+    return this.http.get<multiDaysForcastData>(`${environment.weatherApiBaseUrl}/forecast`,{
+      params: new HttpParams()
+      .set('lat', lat)
+      .set('lon', lon)
       .set('appid', environment.weatherApiKey)
       .set('units', 'metric')
     });
