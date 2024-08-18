108c7e587d8d8bcfdd089604363fa76b878b08c4
nasriime <mnasr4040@gmail.com>
2024-08-18 17:42:33 +0200

    four days weather logic

--- src/app/components/four-days/four-days.component.ts
+++ src/app/components/four-days/four-days.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+ import { multiDaysForcastData, List } from '../../models/multiDaysForcast.model';
 
 @Component({
   selector: 'app-four-days',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './four-days.component.html',
   styleUrl: './four-days.component.scss'
 })
-export class FourDaysComponent {
+export class FourDaysComponent implements OnInit {
+  multiDaysForcastList?: List[] =[];
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getMultiDaysForcast(48.210033, 16.363449).subscribe({
+      next: (data: multiDaysForcastData) => {
+        let fourDaysDates: {[key: string]: boolean} = {};
+
+        data.list.forEach(item => {
+          const date = item.dt_txt.slice(0, 10);
+          if(!fourDaysDates[date]) {
+            fourDaysDates[date] = true;
+            this.multiDaysForcastList?.push(item);
+          }
+        });
+        
+        this.multiDaysForcastList = this.multiDaysForcastList?.slice(1, 5);  
+      }
+    });
+  }
 
 }
