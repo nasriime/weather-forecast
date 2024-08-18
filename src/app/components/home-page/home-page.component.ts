52bb4ab16dfccc44f5d6958b380f05f80dabba2f
nasriime <mnasr4040@gmail.com>
2024-08-18 14:23:41 +0200

    add home page component

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
+import { WeatherService } from '../../services/weather.service';
+import { weatherData } from '../../models/weather.model';
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [DecimalPipe],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent implements OnInit {
+  weatherData?: weatherData;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('vienna').subscribe({
+      next: (data) => {
+        console.log(data);
+        this.weatherData = data;
+      }
+    });
+  }
+}
