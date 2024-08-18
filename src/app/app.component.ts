4c8b810b3c71c420833a50a788deada4dc8b8acf
nasriime <mnasr4040@gmail.com>
2024-08-18 21:12:59 +0200

    add nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DecimalPipe],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
