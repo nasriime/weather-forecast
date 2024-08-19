a03e2e68864e38968c5521b05381ab810271ecf9
nasriime <mnasr4040@gmail.com>
2024-08-20 01:26:50 +0200

    use browser geolocation api

--- src/app/components/four-days/four-days.component.ts
+++ src/app/components/four-days/four-days.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { multiDaysForcastData, List } from '../../models/multiDaysForcast.model';
 import { WeatherCardComponent } from '../weather-card/weather-card.component';
+import { convertCountrryCodeToName } from '../../utils/index';
 
 @Component({
   selector: 'app-four-days',
@@ -13,15 +14,22 @@ import { WeatherCardComponent } from '../weather-card/weather-card.component';
 export class FourDaysComponent implements OnInit {
   multiDaysForecastData?: multiDaysForcastData
   multiDaysForecastList?: List[] =[];
+  countryName?: string = '';
+  locationDenied: boolean = false;
+  errorMsg?: string = '';
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    this.weatherService.getMultiDaysForcast(48.210033, 16.363449).subscribe({
+   this.getLocation()
+  }
+  getMultiDaysForcast(lat: number, lon: number) {
+    this.weatherService.getMultiDaysForcast(lat, lon).subscribe({
       next: (data: multiDaysForcastData) => {
         this.multiDaysForecastData = data;
+        this.countryName = convertCountrryCodeToName(data.city.country);
         let fourDaysDates: {[key: string]: boolean} = {};
-
+  
         data.list.forEach(item => {
           const date = item.dt_txt.slice(0, 10);
           if(!fourDaysDates[date]) {
@@ -34,5 +42,23 @@ export class FourDaysComponent implements OnInit {
       }
     });
   }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {      
+        this.getMultiDaysForcast(position.coords.latitude, position.coords.longitude);
+      }, (error)=> {
+        const errors: { [key: number]: string } = {
+          [error.PERMISSION_DENIED]: 'User denied the request for Geolocation.',
+          [error.POSITION_UNAVAILABLE]: 'Location information is unavailable.',
+          [error.TIMEOUT]: 'The request to get user location timed out.',
+        }
+        if(error.code == error.PERMISSION_DENIED) {
+          this.locationDenied = true
+        }
+        this.errorMsg = errors[error.code];
+      })
+    }
+  } 
 
 }
