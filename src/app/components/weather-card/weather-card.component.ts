7c71632492eb3dcb82f51f4a1cb48566e1a309da
nasriime <mnasr4040@gmail.com>
2024-08-20 20:01:45 +0200

    make show more smooth

--- src/app/components/weather-card/weather-card.component.ts
+++ src/app/components/weather-card/weather-card.component.ts
@@ -1,28 +1,27 @@
 import { Component, Input } from '@angular/core';
-import { weatherData } from '../../models/weather.model';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-weather-card',
   standalone: true,
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, NgClass],
   templateUrl: './weather-card.component.html',
-  styleUrl: './weather-card.component.scss'
+  styleUrl: './weather-card.component.scss',
 })
 export class WeatherCardComponent {
   moreInfo: boolean = false;
 
-  @Input() max?: number; 
+  @Input() max?: number;
   @Input() min?: number;
   @Input() humidity?: number;
-  @Input() temp?: number; 
+  @Input() temp?: number;
   @Input() wind?: number;
   @Input() name?: string;
   @Input() pressure?: number;
   @Input() seaLevel?: number;
   @Input() clouds?: number;
-  @Input() feelsLike?: number;  
-  @Input() date?: string;  
+  @Input() feelsLike?: number;
+  @Input() date?: string;
 
   toggleMoreInfo() {
     this.moreInfo = !this.moreInfo;
